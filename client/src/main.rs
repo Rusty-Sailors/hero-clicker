008cfd8a63d21e06c7c7691dcbe78fa085384e59
MahlerFive <jefflegweak@gmail.com>
2022-10-22 17:42:50 -0400

    add simple client server

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,15 @@
+use std::{collections::HashMap, net::UdpSocket, time::SystemTime, time::Duration};
+use serde::{Deserialize, Serialize};
+
 use bevy::prelude::*;
 use bevy_inspector_egui::WorldInspectorPlugin;
+use bevy_renet::{
+    renet::{
+        ClientAuthentication, RenetClient, RenetConnectionConfig
+    }, RenetClientPlugin
+};
+
+const PROTOCOL_ID: u64 = 7;
 
 mod hero;
 mod gold;
@@ -9,6 +19,8 @@ pub use gold::*;
 
 fn main() {
     App::new()
+    .add_plugin(RenetClientPlugin)
+    .insert_resource(new_renet_client())
     .add_startup_system(spawn_camera)
     .add_plugins(DefaultPlugins)
     .add_plugin(WorldInspectorPlugin::default())
@@ -19,4 +31,19 @@ fn main() {
 
 fn spawn_camera(mut commands: Commands) {
     commands.spawn_bundle(Camera2dBundle::default());
+}
+
+fn new_renet_client() -> RenetClient {
+    let server_addr = "127.0.0.1:5000".parse().unwrap();
+    let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
+    let connection_config = RenetConnectionConfig::default();
+    let current_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+    let client_id = current_time.as_millis() as u64;
+    let authentication = ClientAuthentication::Unsecure {
+        client_id,
+        protocol_id: PROTOCOL_ID,
+        server_addr,
+        user_data: None,
+    };
+    RenetClient::new(current_time, socket, client_id, connection_config, authentication).unwrap()
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,47 @@
+use bevy::prelude::*;
+use bevy_renet::{
+    renet::{
+        RenetConnectionConfig, RenetServer, ServerAuthentication,
+        ServerConfig, ServerEvent,
+        // RenetError,
+    },
+    RenetServerPlugin,
+};
+use std::time::SystemTime;
+use std::{net::UdpSocket};
+// use serde::{Deserialize, Serialize};
+
+const PROTOCOL_ID: u64 = 7;
+
 fn main() {
-    println!("Hello, world!");
+    let server = new_renet_server();
+    let mut app = App::new();
+    app
+        .add_plugins(MinimalPlugins)
+        .add_plugin(RenetServerPlugin)
+        .insert_resource(server)
+        .add_system(handle_server_events_system);
+    app.run();
+}
+
+fn new_renet_server() -> RenetServer {
+    let server_addr = "127.0.0.1:5000".parse().unwrap();
+    let socket = UdpSocket::bind(server_addr).unwrap();
+    let connection_config = RenetConnectionConfig::default();
+    let server_config = ServerConfig::new(64, PROTOCOL_ID, server_addr, ServerAuthentication::Unsecure);
+    let current_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+    RenetServer::new(current_time, server_config, connection_config, socket).unwrap()
+}
+
+fn handle_server_events_system(mut server_events: EventReader<ServerEvent>) {
+    for event in server_events.iter() {
+        match event {
+            ServerEvent::ClientConnected(id, _) => {
+                println!("Client {} connected", id);
+            }
+            ServerEvent::ClientDisconnected(id) => {
+                println!("Client {} disconnected", id);
+            }
+        }
+    }
 }
