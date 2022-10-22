7f76d4e8c30e0375730dd9f2bcc6de3c7aecba8d
MahlerFive <jefflegweak@gmail.com>
2022-10-22 11:52:46 -0400

    initial app setup

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,14 @@
-// use bevy::prelude::*;
+use bevy::prelude::*;
+use bevy_inspector_egui::WorldInspectorPlugin;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .add_startup_system(spawn_camera)
+    .add_plugins(DefaultPlugins)
+    .add_plugin(WorldInspectorPlugin::default())
+    .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn_bundle(Camera2dBundle::default());
 }
