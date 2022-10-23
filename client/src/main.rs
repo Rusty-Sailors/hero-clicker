5853030c146a4ccacbdd532b36855d85f64f7b17
MahlerFive <jefflegweak@gmail.com>
2022-10-23 02:03:17 -0400

    add window settings

--- client/src/main.rs
+++ client/src/main.rs
@@ -21,6 +21,14 @@ pub use multiplayer::*;
 fn main() {
     App::new()
         .add_loopless_state(AppState::MainMenu)
+        .insert_resource(WindowDescriptor {
+            width: 1920.0,
+            height: 1080.0,
+            title: "Hero Clicker".to_string(),
+            resizable: false,
+            present_mode: bevy::window::PresentMode::Fifo,
+            ..Default::default()
+        })
         .add_plugin(CameraPlugin)
         .add_plugins(DefaultPlugins)
         .add_plugin(WorldInspectorPlugin::default())
